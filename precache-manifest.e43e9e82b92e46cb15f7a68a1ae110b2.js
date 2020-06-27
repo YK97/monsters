a89f0becc3d001a60ae7f9939372a1e2a5ed03fc
YuriiK <yura.kostukov@gmail.com>
2020-06-27 22:58:36 +0200

    Updates

---
+++ precache-manifest.e43e9e82b92e46cb15f7a68a1ae110b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47b0c96473a3c018e294f2a46603600",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "81147ec323d98f1e8f5c",
+    "url": "/monsters/static/css/main.7174d0f8.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81147ec323d98f1e8f5c",
+    "url": "/monsters/static/js/main.95f9d88f.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
